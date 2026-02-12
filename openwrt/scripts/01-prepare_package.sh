b6c0f45248b56403de545a26fa874bc7f5f11ad7
tvliving <164112004+tvliving@users.noreply.github.com>
2026-02-12 10:26:51 +0800

    Update 01-prepare_package.sh

--- openwrt/scripts/01-prepare_package.sh
+++ openwrt/scripts/01-prepare_package.sh
@@ -52,20 +52,5 @@ git clone https://$github/timsaya/luci-app-bandix package/new/luci-app-bandix
 # openwrt-bandix
 git clone https://$github/timsaya/openwrt-bandix package/new/openwrt-bandix
 
-# 添加 iStore 源
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
-./scripts/feeds install -d y -p istore luci-i18n-store-zh-cn
-
-# 设默认首页为 iStore
-mkdir -p files/etc/uci-defaults
-cat > files/etc/uci-defaults/99-istore-index <<EOF
-#!/bin/sh
-uci set luci.main.index=/cgi-bin/luci/istore
-uci commit luci
-EOF
-chmod +x files/etc/uci-defaults/99-istore-index
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
